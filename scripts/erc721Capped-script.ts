5154266ed90cf2a6fa3d95eaf61b6021a17329c7
Altais <cervantescedric@gmail.com>
2021-08-19 03:46:41 +0200

    add Erc721Capped

---
+++ scripts/erc721Capped-script.ts
@@ -0,0 +1,21 @@
+// @ts-ignore
+import { run, ethers } from "hardhat"
+
+async function main() {
+
+  await run("compile")
+  const StartonERC721 = await ethers.getContractFactory("StartonERC721Capped");
+
+  const nft = await StartonERC721.deploy("Starton nft", "NFT", "https://nft.starton.io/", 1);
+
+  await nft.deployed();
+  console.log("Contract deployed to:", nft.address);
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
