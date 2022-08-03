62c20d08683e448569240bc3e1a4000206f5be56
startonmateo <mateo@starton.io>
2022-08-03 11:46:42 +0200

    docs: StartonERC721AuctionSale documentation

--- contracts/StartonERC721AuctionSale.sol
+++ contracts/StartonERC721AuctionSale.sol
@@ -5,6 +5,9 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./StartonERC721MetaTransaction.sol";
 
+/// @title StartonERC721AuctionSale
+/// @author Starton
+/// @notice Contract to be able to sell ERC721 tokens through a auction
 contract StartonERC721AuctionSale is Ownable {
     address private immutable _feeReceiver;
 
@@ -16,8 +19,13 @@ contract StartonERC721AuctionSale is Ownable {
     uint256 public endTime;
     bool private _claimed;
 
+    /** @notice Event when a auction started */
     event AuctionStarted(uint256 startTime, uint256 endTime);
+
+    /** @notice Event when a auction winner has claimed his prize */
     event AuctionClaimed(address indexed winner, uint256 price);
+
+    /** @notice Event when a account bided on a auction */
     event Bided(address indexed bidder, uint256 amount);
 
     constructor(
@@ -27,6 +35,7 @@ contract StartonERC721AuctionSale is Ownable {
         uint256 endTime_,
         address feeReceiver
     ) {
+        // Check if the address of the feeReceiver is correct
         require(feeReceiver != address(0), "Fee receiver address is not valid");
         _feeReceiver = feeReceiver;
 
@@ -34,11 +43,18 @@ contract StartonERC721AuctionSale is Ownable {
         currentPrice = startingPrice;
         startTime = startTime_;
         endTime = endTime_;
+
+        // Set inititial states of the auction to no winner and not claimed
         currentAuctionWinner = address(0);
         _claimed = false;
+
+        // Emit the event when the auction starts
         emit AuctionStarted(startTime, endTime);
     }
 
+    /**
+     * @notice Bid for the current auction
+     */
     function bid() public payable {
         require(startTime <= block.timestamp, "Bidding not started");
         require(endTime >= block.timestamp, "Bidding finished");
@@ -49,6 +65,11 @@ contract StartonERC721AuctionSale is Ownable {
         emit Bided(_msgSender(), msg.value);
     }
 
+    /**
+     * @notice Claim the prize of the current auction
+     * @param to The address to send the prize to
+     * @param tokenURI The tokenURI of the token to be sent
+     */
     function safeMint(address to, string memory tokenURI) public {
         require(
             to == currentAuctionWinner,
@@ -61,6 +82,12 @@ contract StartonERC721AuctionSale is Ownable {
         emit AuctionClaimed(to, currentPrice);
     }
 
+    /**
+     * @notice start a new auction for a new NFT
+     * @param startingPrice the starting price of the new auction
+     * @param startTime_ the time when the auction starts
+     * @param endTime_ the time when the auction ends
+     */
     function startNewAuction(
         uint256 startingPrice,
         uint256 startTime_,
@@ -68,14 +95,19 @@ contract StartonERC721AuctionSale is Ownable {
     ) public onlyOwner {
         require(_claimed, "The auction hasn't been claimed yet");
 
+        // Reset the state variables for a new auction to begin
         _claimed = false;
         currentPrice = startingPrice;
         currentAuctionWinner = address(0);
         startTime = startTime_;
         endTime = endTime_;
+
         emit AuctionStarted(startTime, endTime);
     }
 
+    /**
+     * @notice withdraw funds from the smart contract to the feeReceiver
+     */
     function withdraw() public {
         payable(_feeReceiver).transfer(address(this).balance);
     }
