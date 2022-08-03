f04fbea6c62f4736a08c91f13e39fea988e51d94
startonmateo <mateo@starton.io>
2022-08-03 13:07:16 +0200

    docs: StartonERC721WhitelistSale documentation

--- contracts/StartonERC721WhitelistSale.sol
+++ contracts/StartonERC721WhitelistSale.sol
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: MIT
-// StartonERC721WhitelistSale contract: version 0.0.1
-// Creator: https://starton.io
 
 pragma solidity 0.8.9;
 
@@ -9,11 +7,15 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "./StartonERC721MetaTransaction.sol";
 
+/// @title StartonERC721WhitelistSale
+/// @author Starton
+/// @notice Contract that can sell ERC721 tokens through a whitelist sale with a limited avaible supply, start and end time as well as max tokens per address
 contract StartonERC721WhitelistSale is Context {
     using SafeMath for uint256;
 
     address private immutable _feeReceiver;
 
+    // Root of the merkle tree for the whitelisted address
     bytes32 private _merkleRoot;
 
     StartonERC721MetaTransaction public token;
@@ -36,6 +38,7 @@ contract StartonERC721WhitelistSale is Context {
         uint256 maxSupply,
         address feeReceiver
     ) {
+        // Check if the address of the feeReceiver is correct
         require(feeReceiver != address(0), "Fee receiver address is not valid");
         _feeReceiver = feeReceiver;
 
@@ -48,6 +51,12 @@ contract StartonERC721WhitelistSale is Context {
         leftSupply = maxSupply;
     }
 
+    /**
+     * @notice Mint a token to a given address for a price if the given address is whitelisted
+     * @param to The address to mint the token to
+     * @param tokenURI The token metadata URI
+     * @param merkleProof The merkle proof of the address in the whitelist
+     */
     function safeMint(
         address to,
         string memory tokenURI,
@@ -66,10 +75,16 @@ contract StartonERC721WhitelistSale is Context {
         _mint(to, tokenURI);
     }
 
+    /**
+     * @notice Mint multiple tokens to a given address for a price if the given address is whitelisted
+     * @param to The address to mint the token to
+     * @param tokenURIs The token metadata URI array
+     * @param merkleProof The merkle proof of the address in the whitelist
+     */
     function safeBatchMint(
         address to,
         uint256 amount,
-        string[] memory tokenURI,
+        string[] memory tokenURIs,
         bytes32[] calldata merkleProof
     ) public payable {
         bytes32 leaf = keccak256(abi.encodePacked(_msgSender()));
@@ -83,14 +98,22 @@ contract StartonERC721WhitelistSale is Context {
         require(endTime >= block.timestamp, "Minting finished");
 
         for (uint256 i = 0; i < amount; ++i) {
-            _mint(to, tokenURI[i]);
+            _mint(to, tokenURIs[i]);
         }
     }
 
+    /**
+     * @notice Withdraw funds from the smart contract to the feeReceiver
+     */
     function withdraw() public {
         payable(_feeReceiver).transfer(address(this).balance);
     }
 
+    /**
+     * @dev Mint a token to the given address and updates state variables for the sale
+     * @param to The address to mint the token to
+     * @param tokenURI The URI of the token
+     */
     function _mint(address to, string memory tokenURI) internal {
         require(
             tokensClaimed[_msgSender()] < maxTokensPerAddress,
