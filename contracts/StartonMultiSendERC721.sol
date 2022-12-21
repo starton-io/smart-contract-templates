4b55f0d1b990ca01a0e8869f80eb10acc2a87d32
startonmateo <mateo@starton.io>
2022-12-21 12:27:35 +0100

    doc: MultiSendERC721 inline documentation

--- contracts/StartonMultiSendERC721.sol
+++ contracts/StartonMultiSendERC721.sol
@@ -4,7 +4,16 @@ pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
+/// @title Starton Multi Send ERC721
+/// @author Starton
+/// @notice This contract allows to send multiple ERC721 tokens in a single transaction
 contract StartonMultiSendERC721 {
+    /**
+     * @notice Send multiple ERC721 tokens in a single transaction
+     * @param token The address of the ERC721 token
+     * @param ids The ids of tokens to send
+     * @param addresses The addresses to send the tokens to
+     */
     function multiSend(
         address token,
         uint256[] calldata ids,
@@ -17,6 +26,7 @@ contract StartonMultiSendERC721 {
 
         uint256 length = ids.length;
         for (uint256 i = 0; i < length; ) {
+            // Don't check if the transfer is successfull because we still wants to continue to send the other tokens
             IERC721(token).safeTransferFrom(msg.sender, addresses[i], ids[i]);
             unchecked {
                 i++;
