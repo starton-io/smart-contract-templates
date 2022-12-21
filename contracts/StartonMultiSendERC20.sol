5f66e6b1e18485e60591b45a12ae4bf386b2382f
startonmateo <mateo@starton.io>
2022-12-21 12:27:28 +0100

    doc: MultiSendERC20 inline documentation

--- contracts/StartonMultiSendERC20.sol
+++ contracts/StartonMultiSendERC20.sol
@@ -4,7 +4,16 @@ pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/// @title Starton Multi Send ERC20
+/// @author Starton
+/// @notice This contract allows to send multiple ERC20 tokens in a single transaction
 contract StartonMultiSendERC20 {
+    /**
+     * @notice Send multiple ERC20 tokens in a single transaction
+     * @param token The address of the ERC20 token
+     * @param amounts The amounts of tokens to send
+     * @param addresses The addresses to send the tokens to
+     */
     function multiSend(
         address token,
         uint256[] calldata amounts,
@@ -17,6 +26,7 @@ contract StartonMultiSendERC20 {
 
         uint256 length = amounts.length;
         for (uint256 i = 0; i < length; ) {
+            // Don't check if the transfer is successfull because we still wants to continue to send the other tokens
             IERC20(token).transferFrom(msg.sender, addresses[i], amounts[i]);
             unchecked {
                 i++;
