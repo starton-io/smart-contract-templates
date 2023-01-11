ef57b337a3bad804269b17b9a6be71d89f1e60b2
startonmateo <mateo@starton.io>
2023-01-11 15:34:48 +0100

    refactor: change StartonERC721Sale into StartonERC721BaseSale

--- flatten/nft-sales/StartonERC721BaseSale.sol
+++ flatten/nft-sales/StartonERC721BaseSale.sol
@@ -1,269 +1,8 @@
 // Sources flattened with hardhat v2.10.1 https://hardhat.org
 
-// File @openzeppelin/contracts/utils/cryptography/MerkleProof.sol@v4.7.1
-
-// SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v4.7.0) (utils/cryptography/MerkleProof.sol)
-
-pragma solidity ^0.8.0;
-
-/**
- * @dev These functions deal with verification of Merkle Tree proofs.
- *
- * The proofs can be generated using the JavaScript library
- * https://github.com/miguelmota/merkletreejs[merkletreejs].
- * Note: the hashing algorithm should be keccak256 and pair sorting should be enabled.
- *
- * See `test/utils/cryptography/MerkleProof.test.js` for some examples.
- *
- * WARNING: You should avoid using leaf values that are 64 bytes long prior to
- * hashing, or use a hash function other than keccak256 for hashing leaves.
- * This is because the concatenation of a sorted pair of internal nodes in
- * the merkle tree could be reinterpreted as a leaf value.
- */
-library MerkleProof {
-    /**
-     * @dev Returns true if a `leaf` can be proved to be a part of a Merkle tree
-     * defined by `root`. For this, a `proof` must be provided, containing
-     * sibling hashes on the branch from the leaf to the root of the tree. Each
-     * pair of leaves and each pair of pre-images are assumed to be sorted.
-     */
-    function verify(
-        bytes32[] memory proof,
-        bytes32 root,
-        bytes32 leaf
-    ) internal pure returns (bool) {
-        return processProof(proof, leaf) == root;
-    }
-
-    /**
-     * @dev Calldata version of {verify}
-     *
-     * _Available since v4.7._
-     */
-    function verifyCalldata(
-        bytes32[] calldata proof,
-        bytes32 root,
-        bytes32 leaf
-    ) internal pure returns (bool) {
-        return processProofCalldata(proof, leaf) == root;
-    }
-
-    /**
-     * @dev Returns the rebuilt hash obtained by traversing a Merkle tree up
-     * from `leaf` using `proof`. A `proof` is valid if and only if the rebuilt
-     * hash matches the root of the tree. When processing the proof, the pairs
-     * of leafs & pre-images are assumed to be sorted.
-     *
-     * _Available since v4.4._
-     */
-    function processProof(bytes32[] memory proof, bytes32 leaf) internal pure returns (bytes32) {
-        bytes32 computedHash = leaf;
-        for (uint256 i = 0; i < proof.length; i++) {
-            computedHash = _hashPair(computedHash, proof[i]);
-        }
-        return computedHash;
-    }
-
-    /**
-     * @dev Calldata version of {processProof}
-     *
-     * _Available since v4.7._
-     */
-    function processProofCalldata(bytes32[] calldata proof, bytes32 leaf) internal pure returns (bytes32) {
-        bytes32 computedHash = leaf;
-        for (uint256 i = 0; i < proof.length; i++) {
-            computedHash = _hashPair(computedHash, proof[i]);
-        }
-        return computedHash;
-    }
-
-    /**
-     * @dev Returns true if the `leaves` can be proved to be a part of a Merkle tree defined by
-     * `root`, according to `proof` and `proofFlags` as described in {processMultiProof}.
-     *
-     * _Available since v4.7._
-     */
-    function multiProofVerify(
-        bytes32[] memory proof,
-        bool[] memory proofFlags,
-        bytes32 root,
-        bytes32[] memory leaves
-    ) internal pure returns (bool) {
-        return processMultiProof(proof, proofFlags, leaves) == root;
-    }
-
-    /**
-     * @dev Calldata version of {multiProofVerify}
-     *
-     * _Available since v4.7._
-     */
-    function multiProofVerifyCalldata(
-        bytes32[] calldata proof,
-        bool[] calldata proofFlags,
-        bytes32 root,
-        bytes32[] memory leaves
-    ) internal pure returns (bool) {
-        return processMultiProofCalldata(proof, proofFlags, leaves) == root;
-    }
-
-    /**
-     * @dev Returns the root of a tree reconstructed from `leaves` and the sibling nodes in `proof`,
-     * consuming from one or the other at each step according to the instructions given by
-     * `proofFlags`.
-     *
-     * _Available since v4.7._
-     */
-    function processMultiProof(
-        bytes32[] memory proof,
-        bool[] memory proofFlags,
-        bytes32[] memory leaves
-    ) internal pure returns (bytes32 merkleRoot) {
-        // This function rebuild the root hash by traversing the tree up from the leaves. The root is rebuilt by
-        // consuming and producing values on a queue. The queue starts with the `leaves` array, then goes onto the
-        // `hashes` array. At the end of the process, the last hash in the `hashes` array should contain the root of
-        // the merkle tree.
-        uint256 leavesLen = leaves.length;
-        uint256 totalHashes = proofFlags.length;
-
-        // Check proof validity.
-        require(leavesLen + proof.length - 1 == totalHashes, "MerkleProof: invalid multiproof");
-
-        // The xxxPos values are "pointers" to the next value to consume in each array. All accesses are done using
-        // `xxx[xxxPos++]`, which return the current value and increment the pointer, thus mimicking a queue's "pop".
-        bytes32[] memory hashes = new bytes32[](totalHashes);
-        uint256 leafPos = 0;
-        uint256 hashPos = 0;
-        uint256 proofPos = 0;
-        // At each step, we compute the next hash using two values:
-        // - a value from the "main queue". If not all leaves have been consumed, we get the next leaf, otherwise we
-        //   get the next hash.
-        // - depending on the flag, either another value for the "main queue" (merging branches) or an element from the
-        //   `proof` array.
-        for (uint256 i = 0; i < totalHashes; i++) {
-            bytes32 a = leafPos < leavesLen ? leaves[leafPos++] : hashes[hashPos++];
-            bytes32 b = proofFlags[i] ? leafPos < leavesLen ? leaves[leafPos++] : hashes[hashPos++] : proof[proofPos++];
-            hashes[i] = _hashPair(a, b);
-        }
-
-        if (totalHashes > 0) {
-            return hashes[totalHashes - 1];
-        } else if (leavesLen > 0) {
-            return leaves[0];
-        } else {
-            return proof[0];
-        }
-    }
-
-    /**
-     * @dev Calldata version of {processMultiProof}
-     *
-     * _Available since v4.7._
-     */
-    function processMultiProofCalldata(
-        bytes32[] calldata proof,
-        bool[] calldata proofFlags,
-        bytes32[] memory leaves
-    ) internal pure returns (bytes32 merkleRoot) {
-        // This function rebuild the root hash by traversing the tree up from the leaves. The root is rebuilt by
-        // consuming and producing values on a queue. The queue starts with the `leaves` array, then goes onto the
-        // `hashes` array. At the end of the process, the last hash in the `hashes` array should contain the root of
-        // the merkle tree.
-        uint256 leavesLen = leaves.length;
-        uint256 totalHashes = proofFlags.length;
-
-        // Check proof validity.
-        require(leavesLen + proof.length - 1 == totalHashes, "MerkleProof: invalid multiproof");
-
-        // The xxxPos values are "pointers" to the next value to consume in each array. All accesses are done using
-        // `xxx[xxxPos++]`, which return the current value and increment the pointer, thus mimicking a queue's "pop".
-        bytes32[] memory hashes = new bytes32[](totalHashes);
-        uint256 leafPos = 0;
-        uint256 hashPos = 0;
-        uint256 proofPos = 0;
-        // At each step, we compute the next hash using two values:
-        // - a value from the "main queue". If not all leaves have been consumed, we get the next leaf, otherwise we
-        //   get the next hash.
-        // - depending on the flag, either another value for the "main queue" (merging branches) or an element from the
-        //   `proof` array.
-        for (uint256 i = 0; i < totalHashes; i++) {
-            bytes32 a = leafPos < leavesLen ? leaves[leafPos++] : hashes[hashPos++];
-            bytes32 b = proofFlags[i] ? leafPos < leavesLen ? leaves[leafPos++] : hashes[hashPos++] : proof[proofPos++];
-            hashes[i] = _hashPair(a, b);
-        }
-
-        if (totalHashes > 0) {
-            return hashes[totalHashes - 1];
-        } else if (leavesLen > 0) {
-            return leaves[0];
-        } else {
-            return proof[0];
-        }
-    }
-
-    function _hashPair(bytes32 a, bytes32 b) private pure returns (bytes32) {
-        return a < b ? _efficientHash(a, b) : _efficientHash(b, a);
-    }
-
-    function _efficientHash(bytes32 a, bytes32 b) private pure returns (bytes32 value) {
-        /// @solidity memory-safe-assembly
-        assembly {
-            mstore(0x00, a)
-            mstore(0x20, b)
-            value := keccak256(0x00, 0x40)
-        }
-    }
-}
-
-
-// File @openzeppelin/contracts/utils/Context.sol@v4.7.1
-
-// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
-
-pragma solidity ^0.8.0;
-
-/**
- * @dev Provides information about the current execution context, including the
- * sender of the transaction and its data. While these are generally available
- * via msg.sender and msg.data, they should not be accessed in such a direct
- * manner, since when dealing with meta-transactions the account sending and
- * paying for execution may not be the actual sender (as far as an application
- * is concerned).
- *
- * This contract is only required for intermediate, library-like contracts.
- */
-abstract contract Context {
-    function _msgSender() internal view virtual returns (address) {
-        return msg.sender;
-    }
-
-    function _msgData() internal view virtual returns (bytes calldata) {
-        return msg.data;
-    }
-}
-
-
-// File contracts/abstracts/AStartonWhitelist.sol
-
-
-pragma solidity 0.8.9;
-
-
-abstract contract AStartonWhitelist is Context {
-    // Root of the merkle tree for the whitelisted address
-    bytes32 internal _merkleRoot;
-
-    /** @dev Modifier that reverts when the sender is not whitelisted */
-    modifier isWhitelisted(bytes32[] memory merkleProof) {
-        bytes32 leaf = keccak256(abi.encodePacked(_msgSender()));
-        require(MerkleProof.verify(merkleProof, _merkleRoot, leaf), "Invalid proof");
-        _;
-    }
-}
-
-
 // File @openzeppelin/contracts/utils/Strings.sol@v4.7.1
 
+// SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.7.0) (utils/Strings.sol)
 
 pragma solidity ^0.8.0;
@@ -336,6 +75,33 @@ library Strings {
      */
     function toHexString(address addr) internal pure returns (string memory) {
         return toHexString(uint256(uint160(addr)), _ADDRESS_LENGTH);
+    }
+}
+
+
+// File @openzeppelin/contracts/utils/Context.sol@v4.7.1
+
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
     }
 }
 
@@ -658,62 +424,5 @@ contract StartonERC721BaseSale is Context {
         leftSupply -= 1;
         tokensClaimed[_msgSender()] += 1;
         token.mint(to, tokenURI);
-    }
-}
-
-
-// File contracts/nft-sales/StartonERC721WhitelistSale.sol
-
-
-pragma solidity 0.8.9;
-
-
-/// @title StartonERC721WhitelistSale
-/// @author Starton
-/// @notice Sell ERC721 tokens through a whitelist sale with a limited available supply, start and end time as well as max tokens per address
-contract StartonERC721WhitelistSale is StartonERC721BaseSale, AStartonWhitelist {
-    constructor(
-        address definitiveTokenAddress,
-        bytes32 definitiveMerkleRoot,
-        uint256 definitivePrice,
-        uint256 definitiveStartTime,
-        uint256 definitiveEndTime,
-        uint256 definitiveMaxTokensPerAddress,
-        uint256 definitiveMaxSupply,
-        address definitiveFeeReceiver
-    )
-        StartonERC721BaseSale(
-            definitiveTokenAddress,
-            definitivePrice,
-            definitiveStartTime,
-            definitiveEndTime,
-            definitiveMaxTokensPerAddress,
-            definitiveMaxSupply,
-            definitiveFeeReceiver
-        )
-    {
-        _merkleRoot = definitiveMerkleRoot;
-    }
-
-    /**
-     * @notice Mint a token to a given address for a price if the given address is whitelisted
-     * @param to The address to mint the token to
-     * @param merkleProof The merkle proof of the address in the whitelist
-     */
-    function mint(address to, bytes32[] calldata merkleProof) public payable override isWhitelisted(merkleProof) {
-        super.mint(to, new bytes32[](0));
-    }
-
-    /**
-     * @notice Mint multiple tokens to a given address for a price if the given address is whitelisted
-     * @param to The address to mint the token to
-     * @param merkleProof The merkle proof of the address in the whitelist
-     */
-    function mintBatch(
-        address to,
-        uint256 amount,
-        bytes32[] calldata merkleProof
-    ) public payable override isWhitelisted(merkleProof) {
-        super.mintBatch(to, amount, new bytes32[](0));
     }
 }
