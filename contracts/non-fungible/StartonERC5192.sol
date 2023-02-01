c503942d32c5fb8408398eb6532c4af7b9cd95f2
startonmateo <mateo@starton.io>
2023-02-01 13:57:09 +0100

    doc: StartonERC5192 inline documentation

--- contracts/non-fungible/StartonERC5192.sol
+++ contracts/non-fungible/StartonERC5192.sol
@@ -5,9 +5,13 @@ pragma solidity 0.8.9;
 import "../interfaces/IStartonERC5192.sol";
 import "./StartonERC721Base.sol";
 
+/// @title StartonERC5192
+/// @author Starton
+/// @notice ERC5192 token that is locked by default and which is based of the StartonERC721Base
 contract StartonERC5192 is StartonERC721Base, IStartonERC5192 {
     using Counters for Counters.Counter;
 
+    /** @dev Modifier that reverts because the token is locked */
     modifier checkLock() {
         revert("Token locked");
         _;
@@ -29,18 +33,32 @@ contract StartonERC5192 is StartonERC721Base, IStartonERC5192 {
         )
     {}
 
+    /**
+     * @notice Mint a new token to the given address and set the token metadata while minting is not locked
+     * @param to The address that will receive the token
+     * @param uri The URI of the token metadata
+     * @custom:requires MINTER_ROLE
+     */
     function mint(address to, string memory uri) public virtual override {
         super.mint(to, uri);
 
         emit Locked(_tokenIdCounter.current() - 1);
     }
 
+    /**
+     * @notice Check if a token is locked
+     * @param tokenId The ID of the token to check
+     * @return True if the token is locked
+     */
     function locked(uint256 tokenId) public view override returns (bool) {
         require(_exists(tokenId), "Token not found");
 
         return (true);
     }
 
+    /**
+     * @notice Block the safeTransferFrom because it is a SBT
+     */
     function safeTransferFrom(
         address from,
         address to,
@@ -48,22 +66,39 @@ contract StartonERC5192 is StartonERC721Base, IStartonERC5192 {
         bytes memory data
     ) public override checkLock {}
 
+    /**
+     * @notice Block the safeTransferFrom because it is a SBT
+     */
     function safeTransferFrom(
         address from,
         address to,
         uint256 tokenId
     ) public override checkLock {}
 
+    /**
+     * @notice Block the transferFrom because it is a SBT
+     */
     function transferFrom(
         address from,
         address to,
         uint256 tokenId
     ) public override checkLock {}
 
+    /**
+     * @notice Block the approve because it is a SBT
+     */
     function approve(address approved, uint256 tokenId) public override checkLock {}
 
+    /**
+     * @notice Block the setApprovalForAll because it is a SBT
+     */
     function setApprovalForAll(address operator, bool approved) public override checkLock {}
 
+    /**
+     * @dev Call the inherited contract supportsInterface function to know the interfaces as EIP165 says
+     * Implements the IERC5192 interface
+     * @return True if the interface is supported
+     */
     function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
         return interfaceId == type(IStartonERC5192).interfaceId || super.supportsInterface(interfaceId);
     }
